169f332b199b54f1948e55bcb4f794fcc2bc30e9
Rajat <rajatsingh11385@gmail.com>
2026-08-07 06:35:00 +0530

    Question-01(Q.NO-1281)

---
+++ Leetcode_Questions/Question-01.cpp
@@ -0,0 +1,37 @@
+//problem Number 1281
+//Subtract the product and sum of digits of an integer:
+#include<iostream>
+using namespace std;
+int main() {
+    int n = 234;
+    int product = 1;
+    int sum = 0;
+    while(n!=0){
+        int digit = n%10;
+        product = product*digit;
+        sum = sum+digit;
+
+        n = n/10;
+        
+    }
+    int answer = product-sum;
+    cout<< answer;
+
+    //Also Try this on second input And DRY run the code
+/*
+    int n = 456;
+    int product = 1;
+    int sum = 0;
+    while(n!=0){
+        int digit = n%10;
+        product = product*digit;
+        sum = sum+digit;
+
+        n = n/10;
+        
+    }
+    int answer = product-sum;
+    cout<< answer;
+*/
+    return 0;
+}
