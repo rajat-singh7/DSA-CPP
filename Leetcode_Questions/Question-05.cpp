d794365a6269c9f1d641cc613542afb70220269f
Rajat <rajatsingh11385@gmail.com>
2026-08-09 06:08:28 +0530

    Leetcode(Q.NO-231)

---
+++ Leetcode_Questions/Question-05.cpp
@@ -0,0 +1,20 @@
+//Leetcode Problem Number 1009:
+//Problem No-476 as similar as 1009 but in 476 not take n==0 edge case
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter The value of N:" << endl;
+    cin >> n;
+    int m = n;
+    int mask = 0;
+    while (m != 0)
+    {
+        mask = (mask << 1) | 1;
+        m = m >> 1;
+    }
+    int answer = (~n) & mask;
+    cout << answer;
+    return 0;
+}
