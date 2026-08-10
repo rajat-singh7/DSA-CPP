80b151c79819b5f732bbff49bfc27121deef14dc
Rajat <rajatsingh11385@gmail.com>
2026-08-10 22:41:21 +0530

    Leetcode(Q.NO-190)

---
+++ Leetcode_Questions/Question-08.cpp
@@ -0,0 +1,16 @@
+//Leetcode Problem No-190:
+class Solution {
+public:
+    int reverseBits(int n) {
+        unsigned int result = 0; // for Deal with Negative Number we take unsigned int
+        int count = 0;
+        while(count<32){
+            int bit = n&1;
+            result = result<<1;
+            result = result | bit;
+            n = n>>1;
+            count++;
+        }
+        return result;
+    }
+};
