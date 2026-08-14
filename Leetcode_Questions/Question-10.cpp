43c7338165327d53cb44cdeb820d540c505a10ce
Rajat <rajatsingh11385@gmail.com>
2026-08-14 10:28:29 +0530

    Leetcode(Q.NO-1207)

---
+++ Leetcode_Questions/Question-10.cpp
@@ -0,0 +1,35 @@
+//Leetcode Question-1207:
+//Unique Number of Occurence:
+//This is Brute force approach:
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        //Sort the Original Array
+        sort(arr.begin(),arr.end());
+        //Make an array Counts
+        vector<int>counts;
+        int n = arr.size();
+        for(int i = 0;i<n;i++){
+            int count = 1;//Why 1 because i+1 means on same number pointer exists
+            while(i+1<n && arr[i]==arr[i+1]){
+                count++;
+                i++;
+            }
+            //This line for storing the value of count in counts name array
+            counts. push_back(count);
+        }
+        //Now sorting the counts name array
+        sort(counts.begin(),counts.end());
+        for(int j = 0;j<counts.size()-1;j++){
+            if(counts[j]==counts[j+1]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
