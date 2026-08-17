755aeca7aa38ea029047cbf962a70ed6f77d0538
Rajat <rajatsingh11385@gmail.com>
2026-08-17 12:42:30 +0530

    Leetcode(Q.NO-162)

--- Leetcode_Questions/Question-20.cpp
+++ Leetcode_Questions/Question-20.cpp
@@ -1,31 +1,40 @@
-//Leetcode Problem 852:
-//Problem Number 162:
-//Binary Search approach
-//Time Complexity = O(logn)
-#include<iostream>
-#include<vector>
+// Leetcode Problem 162:
+// Find peak Elements:
+// Binary Search approach
+// Time Complexity = O(logn)
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
         int start = 0;
-        int end = arr.size()-1;
-        
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if(arr[mid]<arr[mid+1]){
-                start = mid+1;
+        int end = arr.size() - 1;
+
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1])
+            {
+                start = mid + 1;
             }
             else
             {
                 end = mid;
             }
-            mid = start+(end-start)/2;
+            mid = start + (end - start) / 2;
         }
-       return start;
+        return start;
     }
 };
-int main() {
- return 0;
+int main()
+{
+    Solution s;
+    vector<int> arr = {1, 2, 3, 1};
+    int ans = s.peakIndexInMountainArray(arr);
+    cout << "Peak Element in array at Index: " << ans << endl;
+    return 0;
 }
