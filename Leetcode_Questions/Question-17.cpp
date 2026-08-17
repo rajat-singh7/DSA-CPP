73d051f49f76ec1f397e0b856f4efa6238179271
Rajat <rajatsingh11385@gmail.com>
2026-08-17 12:40:29 +0530

    Leetcode(Q.NO-283)

--- Leetcode_Questions/Question-17.cpp
+++ Leetcode_Questions/Question-17.cpp
@@ -1,28 +1,38 @@
-//leetcode problem - 283
-//Move Zeroes
-//Two pointer approach
-//Two Pointer approach is Best approach for this Problem:
-#include<iostream>
-#include<vector>
+// leetcode problem - 283
+// Move Zeroes
+// Two pointer approach
+// Two Pointer approach is Best approach for this Problem:
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int> &nums)
+    {
         int n = nums.size();
         int i = 0;
         int j = 0;
-        while(j<n){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
+        while (j < n)
+        {
+            if (nums[j] != 0)
+            {
+                swap(nums[i], nums[j]);
                 i++;
             }
             j++;
         }
-       
-        
     }
 };
-int main() {
- return 0;
+int main()
+{
+    Solution s;
+    vector<int> nums = {0, 1, 0, 3, 12};
+    s.moveZeroes(nums);
+    for (int n : nums)
+    {
+        cout << n << " ";
+    }
+    return 0;
 }
