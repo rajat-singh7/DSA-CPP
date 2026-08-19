763a181823bcd3667cf39a4ede22e64aef4af2e5
Rajat <rajatsingh11385@gmail.com>
2026-08-19 19:15:51 +0530

    Leetcode(Q.NO-27)

---
+++ Leetcode_Questions/Question-34.cpp
@@ -0,0 +1,22 @@
+//Leetcode Problem 27:
+//Remove elements:
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index=0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
+            }
+            
+        }
+    return index;
+    }
+};
+int main() {
+ return 0;
+}
