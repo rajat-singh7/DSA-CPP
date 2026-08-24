c820e393feee4017ce6db18ae229296fa870239b
Rajat <rajatsingh11385@gmail.com>
2026-08-24 21:25:04 +0530

    Leetcode(Q.NO-04)

---
+++ Leetcode_Questions/Question-39.cpp
@@ -0,0 +1,34 @@
+
+   
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int> merged(m+n);
+        int i = 0,j =0,k =0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                merged[k++] = nums1[i++];
+            }
+            else
+            {
+                merged[k++] = nums2[j++];
+            }
+        }
+        while(i<m){
+            merged[k++] = nums1[i++];
+        }
+        while(j<n){
+            merged[k++] = nums2[j++];
+        }
+        int size = m+n;
+        if(size%2==1){
+            return merged[size/2];
+        }
+        else
+        {
+            return (merged[(size/2)-1] + merged[size/2])/2.0;
+        }
+    }
+};
