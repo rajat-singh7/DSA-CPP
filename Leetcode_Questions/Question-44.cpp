7c9a4bfabd729468a27755966abeb82a952ab7a9
Rajat <rajatsingh11385@gmail.com>
2026-09-01 22:19:10 +0530

    Leetcode(Q.NO-75)

--- Leetcode_Questions/Question-44.cpp
+++ Leetcode_Questions/Question-44.cpp
@@ -1,5 +1,6 @@
 //Leetcode Problem 75:
 //sort colours:
+//Approach Nummber 01:
 //Based on selection sorting:
 //Time complexity O(n^2):
 #include<iostream>
@@ -19,6 +20,63 @@ public:
             }
             swap(nums[minIndex],nums[i]);
         }   
+    }
+};
+
+//Approach Number 02:
+//Time Complexity For this approach is O(n)
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int count0 = 0, count1 = 0, count2 = 0;
+        //Count the apperance of Numbers:
+        for(int i =0;i<n;i++){
+            if(nums[i]==0) count0++;
+            else if(nums[i]==1) count1++;
+            else count2++;
+        }
+        //Overwrite the Elements of Array:
+        int index= 0;
+        for(int i =0;i<count0;i++){
+            nums[index++] = 0;
+        }
+        for(int i =0;i<count1;i++){
+            nums[index++] = 1;
+        }
+        for(int i =0;i<count2;i++){
+            nums[index++] = 2;
+        }
+        
+    }
+};
+
+//Approach 03:
+//Best approach to solve this problem:
+//Dutch national Flag Algorithm:
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int mid =0;
+        int high = n-1; 
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else
+            {
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+        }
+        
     }
 };
 int main() {
