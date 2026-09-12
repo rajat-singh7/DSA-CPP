520234c94ed6842606ed4a0e4ff89117a23a0746
Rajat <rajatsingh11385@gmail.com>
2026-09-13 05:03:38 +0530

    Leetcode(Q.NO-242)

--- Leetcode_Questions/Question-51.cpp
+++ Leetcode_Questions/Question-51.cpp
@@ -1,7 +1,10 @@
-//Leetcode Problem 
-//isko kal phirse aake update karunga:
+//Leetcode Problem 242:
+//Valid Anagram:
+//Approach Number 01:
+//Time complexity for this --O(nlogn+mlogm):
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 class Solution {
 public:
@@ -9,6 +12,30 @@ public:
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         return s==t;
+    }
+};
+
+
+//Approach Number 02:
+//Best approach:
+//time Complexity--O(n + m) = O(n + n) = O(2n) = O(n):
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        vector<int> h(26);
+        //For String the Frequency
+        for(int i= 0;i<s.size();i++){
+            h[s[i]-'a']++;
+        }
+        //For Erasing the Frequency
+        for(int i= 0;i<t.size();i++){
+            int x = --h[t[i]-'a'];
+            if(x<0) return false;
+        }
+        return true;
     }
 };
 int main() {
