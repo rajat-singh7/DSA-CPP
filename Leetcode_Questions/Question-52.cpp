9c013230d452622e1a55ccaeaeb6760af2bc1942
Rajat <rajatsingh11385@gmail.com>
2026-09-13 21:46:36 +0530

    Leetcode(Q.NO-567)

---
+++ Leetcode_Questions/Question-52.cpp
@@ -0,0 +1,40 @@
+//Leetcode Problem 567:
+//Approach 01:
+//Time complexity for this O(n+m):
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+private:
+    bool isFreqSame(int freq1[],int freq2[]){
+        for(int i = 0;i<26;i++){
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+        for(int i = 0;i<s1.length();i++){
+            freq[s1[i]-'a']++;
+        }
+        int windSize = s1.length();
+        for(int i = 0;i<s2.length();i++){
+            int windIdx = 0,idx = i;
+            int windFreq[26] = {0};
+            while(windIdx < windSize && idx < s2.length()){
+                windFreq[s2[idx]-'a']++;
+                windIdx++;idx++;
+            }
+            if(isFreqSame(freq,windFreq)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+int main() {
+ return 0;
+}
